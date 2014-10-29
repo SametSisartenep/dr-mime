eb89f59808ed4c3e3aeabbbe33bfbe8ce0e74309
SametSisartenep <krashbuffer@gmail.com>
2014-10-29 14:19:56 +0100

    functions defined.

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
 var mimes = require('mimes.json');
 
 function getType ( extension ) {
-
+  return mimes[extension];
 }
 
 function getHeader ( extension ) {
-  
+  return (mimes[extension] + '; charset=utf-8');
 }
 
 function getExt ( type ) {
+  var mimeType;
 
+  for (var i = 0; i < extensions.length; i++)
+  {
+    mimeType = mimes[extensions[i]];
+
+    if (mimeType === type)
+    {
+      return mimeType;
+    }
+  }
 }
 
 // type: e.g 'text/html'
-// extensions: e.g ['.html', '.htm', '.ht']
-function addType ( type, extensions ) {
-
+// extensions: e.g '.html', '.htm'
+function addType ( type, extension ) {
+  mimes[extension] = type;
 }
 
 // typext: type or extension, you chose :P
@@ -26,5 +36,5 @@ function removeType ( typext ) {
 // Apache-like '.types' file
 // E.g: https://svn.apache.org/repos/asf/httpd/httpd/trunk/docs/conf/mime.types
 function loadTypesFile ( file ) {
-
+  
 }
