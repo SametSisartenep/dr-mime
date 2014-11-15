91039d58ffc7e0b7d23837f1e36a8424b40c9063
SametSisartenep <krashbuffer@gmail.com>
2014-11-15 12:07:50 +0100

    v0.6.3. Modularity to improve legibility.

--- index.js
+++ index.js
@@ -1,74 +1,77 @@
 var mimes = require('./mimes.json');
 
-module.exports = exports = {
-  getType: function ( extension ) {
-    var type;
+module.exports.getType = function ( extension ) {
+  var type;
 
-    mimes.map(function ( mime ) {
-      for (var i = 0; i < mime.extension.length; i++)
+  mimes.map(function ( mime ) {
+    for (var i = 0; i < mime.extension.length; i++)
+    {
+      if (mime.extension[i] === extension)
       {
-        if (mime.extension[i] === extension)
-        {
-          type = mime.type;
-        }
+        type = mime.type;
       }
-    });
-    return type || 'text/plain';
-  },
-  getExt: function ( type ) {
-    var extension;
+    }
+  });
+  return type || 'text/plain';
+};
 
+module.exports.getExt = function ( type ) {
+  var extension;
+
+  mimes.map(function ( mime ) {
+    if (mime.type === type)
+    {
+      extension = mime.extension[0];
+    }
+  });
+  return extension || false;
+};
+
+module.exports.getHeader = function ( typext ) {
+  if (typext.indexOf('/') !== -1)
+  {
+    return {'Content-Type' : typext};
+  }
+  else
+  {
+    return {'Content-Type' : this.getType(typext)};
+  }
+};
+
+module.exports.add = function ( type, extension ) {
+  if (!this.getExt(type))
+  {
+    mimes.push({'type' : type, 'extension' : [extension]});
+  }
+  else
+  {
     mimes.map(function ( mime ) {
       if (mime.type === type)
       {
-        extension = mime.extension[0];
+        mime.extension.push(extension);
       }
-    });
-    return extension || false;
-  },
-  getHeader: function ( typext ) {
-    if (typext.indexOf('/') !== -1)
+    }); 
+  }
+};
+
+module.exports.removeType = function ( type ) {
+  for (var i = 0; i < mimes.length; i++)
+  {
+    if (mimes[i].type === type)
     {
-      return {'Content-Type' : typext};
+      mimes.splice(i, 1);
     }
-    else
+  }
+};
+
+module.exports.removeExt = function ( extension ) {
+  for (var i = 0; i < mimes.length; i++)
+  {
+    for (var j = 0; j < mimes[i].extension.length; j++)
     {
-      return {'Content-Type' : this.getType(typext)};
-    }
-  },
-  add: function ( type, extension ) {
-    if (!this.getExt(type))
-    {
-      mimes.push({'type' : type, 'extension' : [extension]});
-    }
-    else
-    {
-      mimes.map(function ( mime ) {
-        if (mime.type === type)
-        {
-          mime.extension.push(extension);
-        }
-      }); 
-    }
-  },
-  removeType: function ( type ) {
-    for (var i = 0; i < mimes.length; i++)
-    {
-      if (mimes[i].type === type)
+      if (mimes[i].extension[j] === extension)
       {
-        mimes.splice(i, 1);
-      }
-    }
-  },
-  removeExt: function ( extension ) {
-    for (var i = 0; i < mimes.length; i++)
-    {
-      for (var j = 0; j < mimes[i].extension.length; j++)
-      {
-        if (mimes[i].extension[j] === extension)
-        {
-          mimes[i].extension.splice(j, 1);
-        }
+        mimes[i].extension.splice(j, 1);
       }
     }
   }
